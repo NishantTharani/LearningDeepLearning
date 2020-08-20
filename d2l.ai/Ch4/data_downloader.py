23868993963cda6cbf87a9edd8df732b8b7a5f24
Nishant Tharani <nishant.tharani@gmail.com>
2020-08-20 19:14:55 +0100

    Deep learning work

---
+++ d2l.ai/Ch4/data_downloader.py
@@ -0,0 +1,53 @@
+import os
+import requests
+import zipfile
+import tarfile
+import hashlib
+from d2l import torch as d2l
+
+DATA_HUB = dict()  # @save
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'  # @save
+
+
+def download(name, cache_dir=os.path.join('..', 'data')):  # @save
+    """Download a file inserted into DATA_HUB, return the local filename."""
+    assert name in DATA_HUB, f"{name} does not exist in {DATA_HUB}."
+    url, sha1_hash = DATA_HUB[name]
+    d2l.mkdir_if_not_exist(cache_dir)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # Hit cache
+    print(f'Downloading {fname} from {url}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+
+def download_extract(name, folder=None):  # @save
+    """Download and extract a zip/tar file."""
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir, ext = os.path.splitext(fname)
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, 'Only zip/tar files can be extracted.'
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+
+def download_all():  # @save
+    """Download all files in the DATA_HUB."""
+    for name in DATA_HUB:
+        download(name)
